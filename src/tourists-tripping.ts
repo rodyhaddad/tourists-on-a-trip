52b50200223e030cc77347fecf313a0f8a248d07
rodyhaddad <rody@rodyhaddad.com>
2015-06-10 16:45:42 +0300

    Implement basic TouristsTrippingApp component

---
+++ src/tourists-tripping.ts
@@ -0,0 +1,12 @@
+/// <reference path="../typings/tsd.d.ts"/>
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+	selector: 'tourists-touring-app'
+})
+@View({
+	templateUrl: 'tourists-tripping.html'
+})
+class TouristsTrippingApp {
+	
+}
