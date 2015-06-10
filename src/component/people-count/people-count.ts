b1afc4ae565631776e14c3ba71ff0100226a4083
rodyhaddad <rody@rodyhaddad.com>
2015-06-10 18:13:07 +0300

    Propogate people-count amount to neighborhood model

--- src/component/people-count/people-count.ts
+++ src/component/people-count/people-count.ts
@@ -1,10 +1,11 @@
 /// <reference path="../../../typings/tsd.d.ts"/>
-import {Component, View, FormBuilder, Control, ControlGroup, onChange, formDirectives} from 'angular2/angular2';
+import {Component, View, FormBuilder, Control, ControlGroup, onChange, formDirectives, EventEmitter} from 'angular2/angular2';
 
 @Component({
 	selector: 'people-count',
 	properties: ['title: tt-title', 'amount: tt-amount'],
-	lifecycles: [onChange]
+	lifecycles: [onChange],
+	events: ['ttAmountChange']
 })
 @View({
 	templateUrl: 'component/people-count/people-count.html',
@@ -17,15 +18,27 @@ export class PeopleCount {
 	amountControl:Control;
 	peopleCountForm:ControlGroup;
 	
+	ttAmountChange: EventEmitter;
+	
 	constructor(builder: FormBuilder) {
 		this.amountControl = new Control()
 		
 		this.peopleCountForm = builder.group({
 			"amount": this.amountControl
 		});
+		
+		this.ttAmountChange = new EventEmitter();
 	}
 	
 	onChange() {
 		this.amountControl.updateValue(this.amount);
+	}
+	
+	reportAmount() {
+		var event = {
+			amount: this.amountControl.value
+		};
+		
+		this.ttAmountChange.next(event);
 	}
 }
